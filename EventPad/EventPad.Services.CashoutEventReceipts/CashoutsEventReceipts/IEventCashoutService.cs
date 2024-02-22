a1023f5aa64c78f63c3d6d655c527db9c5c76a1e
MYP4 <kononovigor2003@gmail.com>
2024-02-22 17:44:58 +0300

    Fix directories

--- EventPad/EventPad.Services.CashoutEventReceipts/CashoutsEventReceipts/IEventCashoutService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EventPad.Services.CashoutEventReceipts;
-
-public interface IEventCashoutService
-{
-    Task<IEnumerable<EventCashoutModel>> GetCashoutReceipts(int page = 1, int pageSize = 10, EventCashoutModelFilter filter = null);
-    Task<EventCashoutModel> GetById(Guid id);
-    Task<EventCashoutModel> Cashout(CreateEventCashoutModel model);
-}
--- EventPad/EventPad.Services.CashoutEventReceipts/CashoutsEventReceipts/Models/EventCashoutModelFilter.cs
+++
@@ -1,9 +0,0 @@
-﻿    namespace EventPad.Services.CashoutEventReceipts;
-
-public class EventCashoutModelFilter
-{
-    public Guid? EventAccount {  get; set; }
-    public Guid? User {  get; set; }
-    public string? BankNumber { get; set; }
-    public DateTime? DateTime { get; set; }
-}
